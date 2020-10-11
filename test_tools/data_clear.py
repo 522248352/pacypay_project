aef803e90856a5ca6035f29743fc43ecb164cbe3
dw <duwan0525@163.com>
2020-10-11 22:03:15 +0800

    相对路径

--- test_tools/data_clear.py
+++ test_tools/data_clear.py
@@ -2,10 +2,13 @@
 import re
 import csv
 from test_tools import rw_csv
+import os
 
+current_path = os.path.dirname(__file__)
+current_path_up = os.path.dirname(current_path)
 
 data = [["transactionId","tradeNo"]]
-with open("../test_data/transaction.csv","w",newline='') as f:
+with open(current_path_up+"/test_data/transaction.csv","w",newline='') as f:
     fs = csv.writer(f)
     for i in data:
         fs.writerow(i)
