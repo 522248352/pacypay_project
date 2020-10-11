d7b518b9b6eb80645a66d6e45e6951824f368205
dw <duwan0525@163.com>
2020-10-11 21:57:22 +0800

    相对路径

--- test_tools/read_yaml.py
+++ test_tools/read_yaml.py
@@ -1,12 +1,15 @@
 import yaml
+import os
 
+current_path = os.path.dirname(__file__)
+current_path_up = os.path.dirname(current_path)
 def get_yamlDataTwo(fileName):
     """
     读取 yaml 文件多数据，获取请求参数
     :param path:
     :return:
     """
-    with open("../test_data/"+fileName, "r" ,encoding="utf-8") as fileData:
+    with open(current_path_up+"/test_data/"+fileName, "r" ,encoding="utf-8") as fileData:
 
         yamlData = yaml.load_all(fileData.read(), Loader=yaml.SafeLoader)
         return yamlData
@@ -17,7 +20,7 @@ def get_yamlDataOne(fileName):
     :param path:
     :return:
     """
-    with open("../test_data/"+fileName, "r" ,encoding="utf-8") as fileData:
+    with open(current_path_up+"/test_data/"+fileName, "r" ,encoding="utf-8") as fileData:
 
         yamlData = yaml.load(fileData.read(), Loader=yaml.SafeLoader)
         return yamlData
