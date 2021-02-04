b53e151c9590a9c98df13d6eb9e35f0d3a8faee8
dw <duwan0525@163.com>
2021-02-04 11:40:39 +0800

    test

---
+++ common/config.py
@@ -0,0 +1,19 @@
+import pytest
+
+dic1 = {'ecpNew': pytest.mark.ecpNew,
+        'ecp': pytest.mark.ecp,
+        'ecpSale': pytest.mark.ecpSale,
+        'safechargeNew': pytest.mark.safechargeNew,
+        'safecharge': pytest.mark.safecharge,
+        'safechargeSale': pytest.mark.safechargeSale,
+        'trustpayNew': pytest.mark.trustpayNew,
+        'trustpay': pytest.mark.trustpay,
+        'trustpaySale': pytest.mark.trustpaySale,
+        'cdNew': pytest.mark.cdNew,
+        'cd': pytest.mark.cd,
+        'cdSale': pytest.mark.cdSale,
+        'ebankNew': pytest.mark.ebankNew,
+        'ebank': pytest.mark.ebank,
+        'ebankSale': pytest.mark.ebankSale,
+
+        }
